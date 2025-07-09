f3b7f191c9d45839e1287e44399521c832e2caef
Satyam Yadav <githubbysatyamaura@gmail.com>
2025-07-09 20:55:48 +0530

    Update play.py

--- BrandrdXMusic/utils/inline/play.py
+++ BrandrdXMusic/utils/inline/play.py
@@ -68,10 +68,10 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="𝐀𝐔𝐑𝐀", url=f"https://t.me/TheAuraNetwork"
+                text="𝐀𝐔𝐑𝐀", url=f"https://t.me/AuraVisual"
             ),
             InlineKeyboardButton(
-                text="sᴜᴘᴘᴏʀᴛ ", url=f"https://t.me/+sRMHl58lWolhMTdl"
+                text="sᴜᴘᴘᴏʀᴛ ", url=f"https://t.me/HeavenChatGroup"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -90,10 +90,10 @@ def stream_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="𝐀𝐔𝐑𝐀", url=f"https://t.me/TheAuraNetwork"
+                text="𝐀𝐔𝐑𝐀", url=f"https://t.me/AuraVisual"
             ),
             InlineKeyboardButton(
-                text="sᴜᴘᴘᴏʀᴛ ", url=f"https://t.me/+sRMHl58lWolhMTdl"
+                text="sᴜᴘᴘᴏʀᴛ ", url=f"https://t.me/HeavenChatGroup"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -245,10 +245,10 @@ def stream_markup2(_, chat_id):
         ],
 [
             InlineKeyboardButton(
-                text="𝐀𝐔𝐑𝐀", url=f"https://t.me/TheAuraNetwork"
+                text="𝐀𝐔𝐑𝐀", url=f"https://t.me/AuraVisual"
             ),
             InlineKeyboardButton(
-                text="sᴜᴘᴘᴏʀᴛ ", url=f"https://t.me/+sRMHl58lWolhMTdl"
+                text="sᴜᴘᴘᴏʀᴛ ", url=f"https://t.me/HeavenChatGroup"
             ),
         ],
 
