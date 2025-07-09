76a993f2a5eb803843270829efa4462c35eba9df
Satyam Yadav <githubbysatyamaura@gmail.com>
2025-07-09 21:21:44 +0530

    Update sudoers.py

--- BrandrdXMusic/plugins/sudo/sudoers.py
+++ BrandrdXMusic/plugins/sudo/sudoers.py
@@ -48,7 +48,7 @@ async def userdel(client, message: Message, _):
 @language
 async def sudoers_list(client, message: Message, _):
     if message.from_user.id not in SUDOERS:
-        return await message.reply_text("💔 <b>ᴏᴡɴᴇʀs:</b>\n1➤ <a href='https://t.me/TheAuraNetwork'>🇮🇳𝐀𝐔𝐑𝐀</a>",
+        return await message.reply_text("💔 <b>ᴏᴡɴᴇʀs:</b>\n1➤ <a href='https://t.me/AuraVisual'>🇮🇳𝐀𝐔𝐑𝐀</a>",
         disable_web_page_preview=True,
         parse_mode="html")
     text = _["sudo_5"]
